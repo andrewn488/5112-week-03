1d5ccad8170c88b74fd384bb2c201e4644068796
Andrew Nalundasan <analundasan@seattleu.edu>
2020-10-06 10:59:59 -0700

    More practice thru all videos of ggplot Part 2. I'm getting the hang of this. And it's kinda fun.

--- 01_Code/week_03_example_videos.R
+++ 01_Code/week_03_example_videos.R
@@ -1,5 +1,6 @@
 # Practice with Week 3 Videos
 # load libraries and datasets to use
+# "gg" = "Grammar of Graphics"
 library(tidyverse)
 data(mpg)
 View(mpg)
@@ -27,3 +28,49 @@ ggplot(data = mpg) +
 # OUTSIDE = setting characteristics for the entire layer
 ggplot(data = mpg) + 
   geom_point(mapping = aes(x = displ, y = hwy), color = "blue")
+
+# histogram
+ggplot(data = mpg) + 
+  geom_histogram(mapping = aes(x = hwy))
+
+# often not where we want to be. binwidth is VERY important for histogram
+# R chooses binwidth = 30 by default
+ggplot(data = mpg) + 
+  geom_histogram(mapping = aes(x = hwy, binwidth = 2))
+
+# color vs. fill
+ggplot(data = mpg) + 
+  geom_bar(mapping = aes(x = class, fill = class))
+# density
+ggplot(data = mpg) + 
+  geom_density(mapping = aes(x = hwy))
+
+# add color to density
+ggplot(data = mpg) + 
+  geom_density(mapping = aes(x = hwy, color = class))
+# add group to density
+ggplot(data = mpg) + 
+  geom_density(mapping = aes(x = hwy, group = class))
+
+# --------------------------------------
+# Global vs. Local Variables
+# Local characteristics are added per layer
+# layers of geoms - point and smooth
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) + 
+  geom_point() + 
+  geom_smooth()
+
+# add characteristics per layer - color to point: 
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) + 
+  geom_point(mapping = aes(color = drv)) + 
+  geom_smooth()
+
+# add characteristics per layer - filter to smooth: 
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) + 
+  geom_point(mapping = aes(color = drv)) + 
+  geom_smooth(data = filter(mpg, drv == "f"))
+
+# grammar of graphics
+
+
+
